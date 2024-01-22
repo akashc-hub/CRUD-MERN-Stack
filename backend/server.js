c9b53d43a5e062e7738dce59123e89157f9cc28e
Akash <akashchauhan84590@gmail.com>
2024-01-22 13:57:24 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+    .connect("mongodb://127.0.0.1:27017/merndb")
+    .then(() => {
+        console.log("Connected Successfully...!");
+
+        app.listen(4000);
+})
+    .catch((error) => {
+        console.log("error", error)
+})
 
 app.get("/", (req, res) => {
     res.send("api running here...!");
 })
 
-app.listen(4000)
