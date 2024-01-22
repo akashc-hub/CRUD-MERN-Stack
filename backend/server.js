cb1190a13c498a5ad19d463f09b51cd2f5edde6f
Akash <akashchauhan84590@gmail.com>
2024-01-22 13:06:53 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("api running");
+    res.send("api running here...!");
 })
 
 app.listen(4000)
